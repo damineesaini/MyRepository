9bbfad2c3594d771d1d9716b2a6f48231a197603
damineesaini <damineesaini11@gmail.com>
2021-02-19 10:36:53 +0530

    updates

--- Java8/code/Ques11.java
+++ Java8/code/Ques11.java
@@ -7,10 +7,12 @@ public class Ques11 {
     public static void main(String[] args){
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
-        int sum=numbers.stream()
-                .map(number-> number * number)
-                .reduce(0,(num1,num2)->num1+num2);
+            double average = numbers.stream()
+                    .mapToInt(number-> number * number)
+                    .average()
+                    .getAsDouble();
 
-        System.out.println("Average of numbers after doubling them is: "+sum/ numbers.size());
+
+        System.out.println("Average of numbers after doubling them is: "+average);
     }
 }
--- Java8/code/Ques9.java
+++ Java8/code/Ques9.java
@@ -7,9 +7,10 @@ public class Ques9 {
     public static void main(String[] args){
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8);
 
-        numbers.stream()
+        List<Integer> even_numbers=numbers.stream()
                 .filter(number->number%2==0)
-                .collect(Collectors.toList())
-                .forEach(System.out::println);
+                .collect(Collectors.toList());
+
+        even_numbers.forEach(System.out::println);
     }
 }
