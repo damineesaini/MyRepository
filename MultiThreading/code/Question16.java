5e63c7a31a69918c32ca399ebd2c0d9b9e2b86c5
damineesaini <damineesaini11@gmail.com>
2021-02-24 13:32:33 +0530

    added word file

---
+++ MultiThreading/code/Question16.java
@@ -0,0 +1,60 @@
+package com.multithreading;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Question16 {
+    private final Lock lock1 = new ReentrantLock(true);
+    private final Lock lock2 = new ReentrantLock(true);
+
+    public void task1(){
+        lock1.lock();
+        System.out.println("lock1 acquired, waiting to acquire lock2.");
+        try {
+            Thread.sleep(50);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+
+        lock2.lock();
+        System.out.println("lock2 acquired");
+
+        System.out.println("executing first operation.");
+
+        lock2.unlock();
+        lock1.unlock();
+    }
+
+    public void task2() {
+        while (true) {
+            try {
+                lock2.tryLock(50, TimeUnit.MICROSECONDS);
+                System.out.println("lock2 acquired, trying to acquire lock1.");
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (lock1.tryLock()) {
+                System.out.println("lock1 acquired.");
+            } else {
+                System.out.println("cannot acquire lock1, releasing lock2.");
+                lock2.unlock();
+                continue;
+            }
+
+            System.out.println("executing second operation.");
+            break;
+        }
+        lock1.unlock();
+        lock2.unlock();
+    }
+
+    public static void main(String[] args) {
+        Question16 deadlock = new Question16();
+        new Thread(deadlock::task1, "T1").start();
+        new Thread(deadlock::task2, "T2").start();
+    }
+}
